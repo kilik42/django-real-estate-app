8b347bc2e89093f9ccbc1561c54964f56ebf09f3
marvin evins <marvinevins@yahoo.com>
2022-11-06 14:29:31 -0600

    working on the form

--- listings/models.py
+++ listings/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import CharField
+
 
 # Create your models here.
 
--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Listing
+from .forms import ListingForm
 
 #CRUD - CREATE, RETRIEVE, UPDATE AND DELETE, LIST(multiple records)
 
@@ -14,6 +15,19 @@ def listing_list(request):
     return render(request, "listings.html", context)
 
 
+def listing_retrieve(request, pk):
+    listing = Listing.objects.get(id=pk)
+    context = {
+        "listing": listing
+    }
+    return render(request, "listing.html", context)
 
 
+
+def listing_create(request):
+    form = ListingForm()
+    context = {
+        "form":form
+    }
+    return render(request, "listing_create.html", context)
 
